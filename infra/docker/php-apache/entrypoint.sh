96d966c97a9bba1493b804d430ac3a8c3831fb4b
june1983 <morotojun@gmail.com>
2024-05-27 14:35:30 +0900

    feat: Update user and group IDs in entrypoint.sh

--- infra/docker/php-apache/entrypoint.sh
+++ infra/docker/php-apache/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-USER_ID=${UID:-1000}
-GROUP_ID=${GID:-1000}
+USER_ID=${WWWUSER:-1000}
+GROUP_ID=${WWWGROUP:-1000}
 USERNAME=${USERNAME:-myuser}
 
 echo "Starting with UID: $USER_ID, GID: $GROUP_ID"
 
-useradd -u $USER_ID -o -m $USERNAME
-groupmod -g $GROUP_ID $USERNAME
+useradd -u "$USER_ID" -o -m "$USERNAME"
+groupmod -g "$GROUP_ID" "$USERNAME"
 
-export HOME=/home/$USERNAME
+export HOME=/home/"$USERNAME"
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
